72979c25cedadab337b0945540b3e74f6c01aac8
André Araujo <andre.cristiano1975@gmail.com>
2020-01-18 22:19:33 +0000

    feat: Add model Dev

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 
 const routes = Router();
 
-routes.post('/users', (req, res) => {
+routes.get('/users', (req, res) => {
     console.log(req.body);
     
     return res.json({
