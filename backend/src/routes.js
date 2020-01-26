08a0ae1d90f1ab78366cfd4de61d216509cf7009
acristh <andre.cristiano1975@gmail.com>
2020-01-26 07:31:15 -0300

    feat: add geolocation on DevSchema

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,13 +5,18 @@ const Dev = require('./models/Dev');
 const routes = Router();
 
 routes.post('/devs', async (req, res) => {
-    const { github_username, techs } = req.body;
+    const { github_username, techs, latitude, longitude } = req.body;
     
     const response = await axios.get(`https://api.github.com/users/${github_username}`);
 
     const { name = login, avatar_url, bio } = response.data;
 
     const techsArray = techs.split(',').map( tech => tech.trim());
+
+    const location = {
+        type: 'Point',
+        coordinates: [longitude, latitude],
+    }
     
     const dev = await Dev.create({
         github_username,
@@ -19,6 +24,7 @@ routes.post('/devs', async (req, res) => {
         avatar_url,
         bio,
         techs: techsArray,
+        location,
     });
 
     return res.json(dev);
