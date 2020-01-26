69b43203ac9f3c95e5cecdf563a6f0e821d6c68d
acristh <andre.cristiano1975@gmail.com>
2020-01-26 07:04:49 -0300

    feat: create /devs post route and save on MongoDB

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,13 +1,27 @@
 const { Router } = require('express');
+const axios = require('axios');
+const Dev = require('./models/Dev');
 
 const routes = Router();
 
-routes.get('/users', (req, res) => {
-    console.log(req.body);
+routes.post('/devs', async (req, res) => {
+    const { github_username, techs } = req.body;
     
-    return res.json({
-        message:'Hello OminiStack 10'
+    const response = await axios.get(`https://api.github.com/users/${github_username}`);
+
+    const { name = login, avatar_url, bio } = response.data;
+
+    const techsArray = techs.split(',').map( tech => tech.trim());
+    
+    const dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray,
     });
+
+    return res.json(dev);
 });
 
 module.exports = routes;
